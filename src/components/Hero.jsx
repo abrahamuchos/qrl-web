f0d9daa148771fbbf798fbef40180d61e93f221e
abrahamuchos <j.abraham29@gmail.com>
2024-11-19 16:58:24 -0400

    Work on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,15 @@ export default function Hero() {
 
 
   return (
-    <div className='hero'>
-      <Lottie animationData={trazoHome} loop={false} width={'100%'}/>
+    // <div className='h-[90vh] bg-Dark-blue hero relative text-white'>
+    //   <Lottie animationData={trazoHome} loop={false} width={'100%'}/>
+    // </div>
+
+
+
+    <div className="w-full h-[90vh] relative bg-Dark-blue text-white overflow-hidden rounded-br-[70px]">
+      <Lottie animationData={trazoHome} loop={true} width={'100%'}/>
+      <div className="absolute bg-white w-[50%] h-[90px] bottom-0 rounded-tr-[70px]"></div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Hero from "../components/Hero.jsx";
-import Navigation from "../components/Navigation.jsx";
 
 export default function Home() {
 
   return (
-    <section className='w-full h-[90vh] bg-Dark-blue'>
-      {/*<Navigation/>*/}
+    <section className='w-full'>
       <Hero/>
       <div className="h-[200vh]"></div>
     </section>
